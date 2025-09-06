e12602dfdf0280ff445fac284ad5253be2906904
somkheartk <43743072+somkheartk@users.noreply.github.com>
2025-09-06 18:34:21 +0700

    - init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersService } from './users/users.service';
+import { UsersController } from './users/users.controller';
+import { ProductsService } from './products/products.service';
+import { ProductsController } from './products/products.controller';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, ProductsModule, UsersModule],
+  controllers: [AppController, UsersController, ProductsController],
+  providers: [AppService, UsersService, ProductsService],
 })
 export class AppModule {}
